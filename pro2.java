bc442491191fbaf1d10e01e09fb7053943947db0
ningaraddi14 <yallakkanavarningaraddi55@gmail.com>
2023-11-20 19:49:00 +0530

    first

---
+++ pro2.java
@@ -0,0 +1,17 @@
+class pro2
+{
+public static void main(String arg[])
+{
+int a,b;
+a=5; b=10;
+System.out.println("add" + (a+b));
+System.out.println("sub"+ (a-b));
+System.out.println("div" + (a/b));
+System.out.println("mul" + (a*b));
+System.out.println("modlus" + (b%a));
+
+
+
+
+}
+}
